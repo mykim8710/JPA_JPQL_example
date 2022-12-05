525be3683150b99680a3edbad1d538f5831f05bb
mykim <mykim8710@gmail.com>
2022-12-05 21:06:03 +0900

    [ ADD ] Java ORM JPA - JPQL study Project

---
+++ src/main/java/hello/jpa/jpql/JpqlMain.java
@@ -0,0 +1,32 @@
+package hello.jpa.jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpqlMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hellojpql");
+        // EntityManagerFactory app 로딩시점에 딱 하나만 만든다.
+
+        EntityManager em = emf.createEntityManager();
+        // 트랜젝션 단위(db 커넥션을 얻어서 쿼리를 날리고 결과를 얻는)로 EntityManager를 만들어야줘야 한다.
+        // 쉽게 생각하면 db 커넥션을 하나 얻은 것이다.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // db 트랜젝션을 시작
+
+        try {
+
+
+            tx.commit();    // 커밋
+        }catch (Exception e) {
+            tx.rollback();  // 롤백
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/hello/jpa/jpql/entity/Member.java
@@ -0,0 +1,25 @@
+package hello.jpa.jpql.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter @Setter
+@Entity
+public class Member { // N
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String username;
+
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team; // 1
+}
